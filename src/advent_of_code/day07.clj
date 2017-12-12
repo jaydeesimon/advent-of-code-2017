969bea6254fdbd93277fa064ce19e9cc39b95ee3
Jeffrey Simon <jdsimon@gmail.com>
2017-12-11 23:39:42 -0500

    Day 7 Part 2

--- src/advent_of_code/day07.clj
+++ src/advent_of_code/day07.clj
@@ -4,6 +4,9 @@
             [clojure.set :as set]))
 
 (def input (str/split-lines (slurp (io/resource "day-07-input.txt"))))
+(def input2 (str/split-lines (slurp (io/resource "day-07-input2.txt"))))
+
+(def test-input (str/split-lines "pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\nqoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\nugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)"))
 
 (defn parse-line [line]
   (let [re (if (str/includes? line "->")
@@ -35,10 +38,64 @@
     (when (= (count root) 1)
       (first root))))
 
+#_(defn sum [node {:keys [weights adj-list] :as towers}]
+  (if-let [children (get adj-list node)
+           children-sums (map #(sum % towers) children)]
+    ))
+
+(defn sum [node {:keys [weights adj-list] :as towers}]
+  (loop [sum (get weights node 0)
+         lst (get adj-list node [])]
+    (if (empty? lst)
+      sum
+      (let [[frst & rst] lst]
+        (recur
+          (+ sum (get weights frst 0))
+          (concat rst (get adj-list frst [])))))))
+
+(defn balances [{:keys [weights adj-list] :as towers}]
+  (map (fn [[node node-child]]
+            (vector node node-child (sum node-child towers)))
+          (for [node (keys weights)
+                node-child (get adj-list node [])]
+            [node node-child])))
+
 
 (comment
 
   ;; part one
   (find-root (parse-input input))
+
+  (def towers (parse-input input))
+  (def test-towers (parse-input test-input))
+  (def towers2 (parse-input input2))
+
+
+  (let [towers (parse-input input)
+        root (find-root towers)]
+    (sum root towers))
+
+  (sum "urbkrn" (parse-input input))
+
+  (->> (balances (parse-input input))
+       (group-by first)
+       (vals)
+       (filter (fn [tuples]
+                 (apply not= (map #(nth % 2) tuples)))))
+
+  (->> (balances (parse-input (str/split-lines (slurp (io/resource "day-07-input2.txt")))))
+       (group-by first)
+       (vals)
+       (filter (fn [tuples]
+                 (apply not= (map #(nth % 2) tuples)))))
+
+  (->> (balances (parse-input test-input))
+       (group-by first)
+       (vals)
+       (filter (fn [tuples]
+                 (apply not= (map #(nth % 2) tuples)))))
+
+  (sum "ztjquwi" (parse-input input))
+
 
   )
