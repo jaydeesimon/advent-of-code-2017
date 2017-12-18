f995ed781c0f69456652dd44cd675ab59de9f8fe
Jeffrey Simon <jdsimon@gmail.com>
2017-12-18 07:49:46 -0500

    Add lengths and fix bug

--- src/advent_of_code/day10.clj
+++ src/advent_of_code/day10.clj
@@ -1,5 +1,10 @@
-(ns advent-of-code.day10)
+(ns advent-of-code.day10
+  (:require [clojure.string :as str]))
 
+(def input "34,88,2,222,254,93,150,0,199,255,39,32,137,136,1,167")
+
+(def lengths (->> (str/split input #",")
+                  (map #(Integer/parseInt %))))
 
 (defn rotate-left [coll n]
   (take (count coll) (drop n (cycle coll))))
@@ -7,6 +12,7 @@
 (defn rotate-right [coll n]
   (rotate-left coll (- (count coll) n)))
 
+;; BREAKS WHEN START IS EQUAL TO END
 (defn hash-step [coll start end]
   (let [rotated (rotate-left coll start)
         [section & remainder] (partition-all (- end start) rotated)
@@ -29,6 +35,13 @@
             rst))))))
 
 (comment
+
+  ;; TODO: Breaks when length is zero
+  ;; Part 1
+  (hash-knot (range 256) lengths)
+
+  (hash-knot (range 256) [300 300 300 300 254 2 88 32 137])
+
 
 
 
