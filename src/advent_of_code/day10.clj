d5e14e88970b580badc30a8ed5eab21518db5dd4
Jeffrey Simon <jdsimon@gmail.com>
2017-12-19 23:20:17 -0500

    Day 10 Part 2

--- src/advent_of_code/day10.clj
+++ src/advent_of_code/day10.clj
@@ -36,17 +36,47 @@
             (inc skip-size)
             rst))))))
 
+(defn str->ascii [s]
+  (vec (.getBytes s "US-ASCII")))
+
+(defn str->lengths [s]
+  (vec (concat (str->ascii s) [17 31 73 47 23])))
+
+(defn hash-knot-part2 [{:keys [coll curr-pos skip-size]} lengths]
+  (loop [coll coll
+         curr-pos curr-pos
+         skip-size skip-size
+         lengths lengths]
+    (let [[length & rst] lengths]
+      (if (nil? length)
+        {:coll coll
+         :curr-pos curr-pos
+         :skip-size skip-size}
+        (let [curr-pos-normalized (mod curr-pos (count coll))]
+          (recur
+            (hash-step coll curr-pos-normalized (+ curr-pos-normalized length))
+            (+ curr-pos length skip-size)
+            (inc skip-size)
+            rst))))))
+
+(defn part-two [coll lengths n]
+  (let [x {:coll coll :curr-pos 0 :skip-size 0}]
+    (take (inc n) (iterate #(hash-knot-part2 % lengths) x))))
+
+
 (comment
 
   ;; Part 1
   (let [[frst scnd] (hash-knot (range 256) lengths)]
     (* frst scnd))
 
-  
 
-
-
-
+  ;; Part 2
+  (let [sparse-hash (:coll (last (part-two (range 256) (str->lengths input) 64)))]
+    (->> (partition 16 sparse-hash)
+         (map #(apply bit-xor %))
+         (map #(format "%02x" %))
+         (apply str)))
 
 
   )
