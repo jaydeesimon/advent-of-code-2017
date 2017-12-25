b712f6165fed9033d5c7d3f751e74c104c0e6073
Jeffrey Simon <jdsimon@gmail.com>
2017-12-25 05:30:02 -0500

    Day 14 Part 2

--- src/advent_of_code/day14.clj
+++ src/advent_of_code/day14.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code.day14
   (:require [advent-of-code.util.knot-hash :refer [knot-hash]]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (defn hex-digit->binary [hd]
   (let [bs (-> (Integer/valueOf (str hd) 16)
@@ -24,6 +25,39 @@
                (binary->grid))))
        (range 128)))
 
+(defn neighbors [grid on]
+  (fn [coord]
+    (->> (map #(mapv + coord %) [[0 1] [1 0] [-1 0] [0 -1]])
+         (filter #(= on (get-in grid %))))))
+
+(defn explore-region [grid coord]
+  (let [neighbors-fn (neighbors grid \#)]
+    (loop [frontier [coord]
+           visited #{coord}]
+      (let [[coord & frontier] frontier]
+        (if (nil? coord)
+          visited
+          (recur (into frontier (->> (neighbors-fn coord)
+                                     (remove visited)))
+                 (into visited [coord])))))))
+
+(defn grid-coords [grid]
+  (for [x (range (count grid))
+        y (range (count (first grid)))]
+    [x y]))
+
+(defn on-coords [grid on]
+  (filter #(= on (get-in grid %)) (grid-coords grid)))
+
+(defn find-all-regions [grid]
+  (loop [coords-unexplored (set (on-coords grid \#))
+         regions []]
+    (if (empty? coords-unexplored)
+      regions
+      (let [region (explore-region grid (first coords-unexplored))]
+        (recur (set/difference coords-unexplored (set region))
+               (conj regions region))))))
+
 
 (comment
 
@@ -32,5 +66,12 @@
        (map (fn [row]
               (count (filter #(= % \#) row))))
        (reduce +))
+
+  ;; Part 2
+  (->> (generate-grid "wenycdww")
+       (mapv vec)
+       (find-all-regions)
+       (count))
+
 
   )
