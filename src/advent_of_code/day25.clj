7c44f5ff582963d0ff962f3e8980e18e61f2d400
Jeffrey Simon <jdsimon@gmail.com>
2018-01-13 12:43:04 -0500

    Day 25 Cleanup

--- src/advent_of_code/day25.clj
+++ src/advent_of_code/day25.clj
@@ -1,68 +1,68 @@
 (ns advent-of-code.day25
   (:require [clojure.set :as set]))
 
-(defn set-one [position ones]
-  (fn [ones]
-    (set/union ones #{position})))
+(defn set-one [position one?]
+  (fn [one?]
+    (set/union one? #{position})))
 
-(defn set-zero [position ones]
-  (fn [ones]
-    (set/difference ones #{position})))
+(defn set-zero [position one?]
+  (fn [one?]
+    (set/difference one? #{position})))
 
-(defn transition-state [{:keys [state position ones] :as world}]
+(defn transition-state [{:keys [state position one?] :as world}]
   (condp = state
-    :a (if (not (ones position))
+    :a (if (not (one? position))
          (-> world
-             (update :ones (set-one position ones))
+             (update :one? (set-one position one?))
              (update :position inc)
              (assoc :state :b))
          (-> world
-             (update :ones (set-zero position ones))
+             (update :one? (set-zero position one?))
              (update :position dec)
              (assoc :state :c)))
-    :b (if (not (ones position))
+    :b (if (not (one? position))
          (-> world
-             (update :ones (set-one position ones))
+             (update :one? (set-one position one?))
              (update :position dec)
              (assoc :state :a))
          (-> world
-             (update :ones (set-one position ones))
+             (update :one? (set-one position one?))
              (update :position inc)
              (assoc :state :d)))
-    :c (if (not (ones position))
+    :c (if (not (one? position))
          (-> world
-             (update :ones (set-zero position ones))
+             (update :one? (set-zero position one?))
              (update :position dec)
              (assoc :state :b))
          (-> world
-             (update :ones (set-zero position ones))
+             (update :one? (set-zero position one?))
              (update :position dec)
              (assoc :state :e)))
-    :d (if (not (ones position))
+    :d (if (not (one? position))
          (-> world
-             (update :ones (set-one position ones))
+             (update :one? (set-one position one?))
              (update :position inc)
              (assoc :state :a))
          (-> world
-             (update :ones (set-zero position ones))
+             (update :one? (set-zero position one?))
              (update :position inc)
              (assoc :state :b)))
-    :e (if (not (ones position))
+    :e (if (not (one? position))
          (-> world
-             (update :ones (set-one position ones))
+             (update :one? (set-one position one?))
              (update :position dec)
              (assoc :state :f))
          (-> world
-             (update :ones (set-one position ones))
+             (update :one? (set-one position one?))
              (update :position dec)
              (assoc :state :c)))
-    :f (if (not (ones position))
+    :f (if (not (one? position))
          (-> world
-             (update :ones (set-one position ones))
+             (update :one? (set-one position one?))
              (update :position inc)
              (assoc :state :d))
          (-> world
-             (update :ones (set-one position ones))
+             (update :one? (set-one position one?))
              (update :position inc)
              (assoc :state :a)))
     (throw (ex-info "" {:unknown-state state}))))
@@ -73,12 +73,12 @@
   ;; Part 1
   (let [initial-world {:state    :a
                        :position 0
-                       :ones     #{}}]
+                       :one?     #{}}]
     (->> initial-world
          (iterate transition-state)
          (take (inc 12481997))
          (last)
-         (:ones)
+         (:one?)
          (count)))
 
   )
